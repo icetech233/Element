5e86121da5ad47bfcfe813f6b517537c4611d6b3
icetech233 <cnzjhanzhouhb@hotmail.com>
2023-02-05 18:12:46 +0800

    add config file

--- src/Element.Core/Controls/Attach/BorderElement.cs
+++ src/Element.Core/Controls/Attach/BorderElement.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
-using System.Windows.Media;
 
 namespace Element.Core.Controls
 {
@@ -27,35 +26,4 @@ namespace Element.Core.Controls
 
         #endregion
     }
-
-    public class IconElement
-    {
-        #region Icon 
-
-        public static readonly DependencyProperty GeometryProperty = 
-            DependencyProperty.RegisterAttached("Geometry", typeof(Geometry), typeof(IconElement), new PropertyMetadata(default(Geometry)));
-
-        public static void SetGeometry(DependencyObject element, Geometry value)
-            => element.SetValue(GeometryProperty, value);
-
-        public static Geometry GetGeometry(DependencyObject element)
-            => (Geometry)element.GetValue(GeometryProperty);
-        #endregion
-
-        // Side Length
-
-        #region Length  Side Length
-
-        public static readonly DependencyProperty LengthProperty =
-            DependencyProperty.RegisterAttached("Length", typeof(double), typeof(IconElement), new PropertyMetadata(double.NaN));
-
-        public static void SetLength(DependencyObject element, double value)
-            => element.SetValue(LengthProperty, value);
-
-        public static double GetLength(DependencyObject element)
-            => (double)element.GetValue(LengthProperty);
-
-        #endregion
-    }
-
 }
---
+++ src/Element.Core/Controls/Attach/IconElement.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Element.Core.Controls
+{
+    public class IconElement
+    {
+        #region Icon 
+
+        public static readonly DependencyProperty GeometryProperty = 
+            DependencyProperty.RegisterAttached("Geometry", typeof(Geometry), typeof(IconElement), new PropertyMetadata(default(Geometry)));
+
+        public static void SetGeometry(DependencyObject element, Geometry value)
+            => element.SetValue(GeometryProperty, value);
+
+        public static Geometry GetGeometry(DependencyObject element)
+            => (Geometry)element.GetValue(GeometryProperty);
+        #endregion
+
+        // Side Length
+
+        #region Length  Side Length
+
+        public static readonly DependencyProperty LengthProperty =
+            DependencyProperty.RegisterAttached("Length", typeof(double), typeof(IconElement), new PropertyMetadata(double.NaN));
+
+        public static void SetLength(DependencyObject element, double value)
+            => element.SetValue(LengthProperty, value);
+
+        public static double GetLength(DependencyObject element)
+            => (double)element.GetValue(LengthProperty);
+
+        #endregion
+    }
+
+}
